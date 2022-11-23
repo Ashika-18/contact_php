3d3186cbe1836da1453cac57f394030afd660147
ashiba <ashibasama@gmail.com>
2022-11-23 20:19:58 +0900

    next 投稿一覧

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+require('dbconnect.php');
+
+if ($_POST['email'] != '' && $_POST['pass'] != '') {
+
+    $login = $db->prepare('SELECT * FROM users WHERE email=? AND password=?');
+    $login->execute(array(
+        $_POST['email'],
+        sha1($_POST['pass'])
+    ));
+    $member = $login->fetch();
+
+    if ($member) {
+        $_SESSION['id'] = $member['id'];
+        $_SESSION['time'] = time();
+
+        header('Location: posts');
+        exit();
+    }
+}
+?>
